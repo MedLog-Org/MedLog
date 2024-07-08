b30a3978295eacbd2bd911b440e05658b842e389
skrsumit250 <skrsumit250@gmail.com>
2024-07-08 23:36:57 +0530

    add database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const PORT = 5000;
+
+require('dotenv').config();
+const URI = process.env.DB_URI;
+const connect=mongoose.connect(URI);
+connect.then(()=>{
+    console.log("DB Connected");
+})
+.catch((err)=>{
+    console.log("DB Not Connected",err);
+})
 app.get('/',(req,res)=>{
     res.send('Server is running');
 });
