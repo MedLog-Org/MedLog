00c48de2a12ce3066d8e1fa2c5deb04dcbcd36cd
skrsumit250 <skrsumit250@gmail.com>
2024-07-15 15:47:18 +0530

    Update Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
-
+import Navbar from './assets/Navbar'
 function App() {
   return (
     <>
+      <Navbar></Navbar>
     </>
   )
 }
---
+++ client/src/assets/Navbar.jsx
@@ -0,0 +1,9 @@
+import '../styles/Navbar.css';
+function Navbar(){
+    return(
+        <div className="Navbar">
+            <h1>MedLog</h1>
+        </div>
+    )
+}
+export default Navbar;
