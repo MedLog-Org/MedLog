eae789d1a9dc99ae81f277480f5dc768333a23d9
skrsumit250 <skrsumit250@gmail.com>
2024-07-17 16:51:03 +0530

    Made Navbar

--- client/src/assets/Navbar.jsx
+++ client/src/assets/Navbar.jsx
@@ -1,9 +1,20 @@
 import '../styles/Navbar.css';
 function Navbar(){
     return(
-        <div className="Navbar">
-            <h1>MedLog</h1>
+        <div className="Home">
+            <img src="/BG.png" alt="" />
+            <div className="Navbar">
+                <h1>MedLog</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>Appointments</li>
+                    <li>Pharmacy</li>
+                    <li>Help</li>
+                    <li id="login-btn">Sign in</li>
+                </ul>
+            </div>
         </div>
+        
     )
 }
 export default Navbar;
