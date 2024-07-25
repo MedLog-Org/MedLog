ca22cc3482346d2f0e5dc6a842d4350755e7fdf8
skrsumit250 <skrsumit250@gmail.com>
2024-07-25 11:49:08 +0530

    made login page

--- client/src/assets/Login.jsx
+++ client/src/assets/Login.jsx
@@ -1,15 +1,18 @@
 import '../styles/Login.css'
+import {Link} from 'react-router-dom'
 function Login(){
     return(
         <div className="Login">
             <div className="login-card">
-                <div className="cross"><img src="/cross.png"/></div>
+                <div className="cross"><Link to={'/'}><img src="/cross.png"/></Link></div>
                 <input type="email" placeholder='Email' required />
                 <input type="password" placeholder='Password' required />
                 <button>Login/Sign in</button>
                 
+                <div className="or"> <hr /><p>Or</p><hr /> </div>
+
                 <div className="google">
-                    <img src="/google.png" alt="" id='google'/>
+                    <img src="/google.png" alt=""/>
                     <p>Login with Google</p>
                 </div>
             </div>
