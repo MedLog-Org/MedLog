e8074f0799b695e28a065bf25397aab84b79146e
skrsumit250 <skrsumit250@gmail.com>
2024-07-26 10:10:25 +0530

    toggle added

--- client/src/assets/Navbar.jsx
+++ client/src/assets/Navbar.jsx
@@ -2,6 +2,7 @@ import '../styles/Navbar.css'
 import {Link} from 'react-router-dom'
 function Navbar(){
     return(
+        <>
         <div className="Navbar">
             <h1>MedLog</h1>
             <ul>
@@ -12,6 +13,17 @@ function Navbar(){
                 <li><Link to="/login" id='btn'>Sign in</Link></li>
             </ul>
         </div>
+        <div className="toggle">
+        <h1>MedLog</h1>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/appointments">Appointments</Link></li>
+                <li><Link to="/pharmacy">Pharmacy</Link></li>
+                <li><Link to="/help">Help</Link></li>
+                <li><Link to="/login" id='btn'>Sign in</Link></li>
+            </ul>
+        </div>
+        </>
     )
 }
 export default Navbar;
