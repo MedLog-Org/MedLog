014cbdc38a9c3722f2c8a53548d353227c3a8b14
skrsumit250 <skrsumit250@gmail.com>
2024-08-04 23:33:25 +0530

    fix typos

--- client/src/assets/Doc/Doc.jsx
+++ client/src/assets/Doc/Doc.jsx
@@ -21,13 +21,12 @@ function Doc(){
               credentials: 'include',
           });
           const result = await response.json();
-          console.log(result.doctor);
-          const doctor = result.doctor;
+          console.log(result.user);
+          const doctor = result.user;
           setName(doctor.name);
           setEmail(doctor.email);
           setPhone(doctor.phone);
-          setSpeciality(doctor.speciality);
-          //setBloodGroup(user.bloodGroup);      
+          setSpeciality(doctor.speciality);     
           } catch (err) {
           console.error(err);
           }
