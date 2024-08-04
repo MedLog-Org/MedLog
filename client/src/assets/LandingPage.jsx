22834c276b68c404815681419b64481e0eb8e94d
skrsumit250 <skrsumit250@gmail.com>
2024-08-04 17:03:03 +0530

    set appointment button

--- client/src/assets/LandingPage.jsx
+++ client/src/assets/LandingPage.jsx
@@ -1,13 +1,18 @@
 import Navbar from "./Navbar";
 import '../styles/LandingPage.css';
 import Footer from "./Footer";
+import { useNavigate } from "react-router-dom";
 function LandingPage(props){
+    const navigate = useNavigate();
+    const gotoAppointment = ()=>{
+        navigate('/appointments')
+    }
     return(
         <>
             <div className="Home">
                 <img src="/BG.png" id="bg"/>
                 <Navbar data={props.data}></Navbar>
-                <button>Book Appointment</button>
+                <button onClick={gotoAppointment}>Book Appointment</button>
                 <img src="/BG2.png" id="bg"/>
                 <img src="/BG3.png" id="bg" />
             </div>
