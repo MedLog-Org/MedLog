cca61e5a305cf136bad62d525cb695af1dd9e540
skrsumit250 <skrsumit250@gmail.com>
2024-08-16 00:09:19 +0530

    fix routes

--- server/server.js
+++ server/server.js
@@ -5,18 +5,20 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const MongoStore = require('connect-mongo');
 
-// Routes
+//  Routes
+const user_collection = require("./models/user");
+const doc_collection = require("./models/doctor");
 const loginRoutes = require('./routes/login')
 const profileRoutes = require('./routes/profile')
 const slotsRoutes = require('./routes/slots')
 const appointmentRoutes = require('./routes/appointments')
-const sessionRoutes = require('./routes/session')
+
 
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials:true
+    origin: 'http://localhost:5173',
+    credentials:true
 }));
 app.use(bodyParser.json());
 const PORT = 8000;
@@ -33,28 +35,57 @@ mongoose.connect(URI)
   });
 
 const store = MongoStore.create({
-  mongoUrl: URI,
-  collectionName: 'sessions',
-  ttl: 24 * 60 * 60
+    mongoUrl: URI,
+    collectionName: 'sessions',
+    ttl: 24 * 60 * 60
 });
 app.use(session({
-  secret: 'secret_key',
-  store: store,
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false, maxAge: 3600000, httpOnly: true},
+    secret: 'secret_key',
+    store: store,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, maxAge: 3600000, httpOnly: true},
 }));
 
-//  login/logout Routes
-app.use(loginRoutes);
-//  profile doc/user Routes
-app.use(profileRoutes);
-//  slots/doc /bookslot Routes
-app.use(slotsRoutes);
-//  appointment slots/bookslot Routes
-app.use(appointmentRoutes);
-//  session Routes
-app.use(sessionRoutes);
+app.use(loginRoutes);       //  login/logout Routes
+app.use(profileRoutes);     //  profile doc/user Routes
+app.use(slotsRoutes);       //  slots/doc /bookslot
+app.use(appointmentRoutes)  //  appointment slots/bookslot
+
+app.get('/', async (req, res) => {
+    try {
+        if (req.session.loggedIn) {
+          console.log(req.session);
+          let collection;
+          if(req.session.userType=='patient')
+            collection=user_collection;
+          else if(req.session.userType=='doctor')
+            collection=doc_collection;
+          else
+            console.log(req.session.userType);
+
+          const user = await collection.findOne({ _id: req.session.userId });
+          console.log(user);
+          res.json({
+            isLoggedIn: true,
+            user: user
+          });
+      } 
+      else {
+          res.json({
+            isLoggedIn: false,
+            message: "User is not logged in"
+          });
+      }
+    } 
+    catch(error){
+        console.error('Error fetching user:', error);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`App is listening on http://localhost:${PORT}`);
