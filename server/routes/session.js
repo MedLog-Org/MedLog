b93ea72e033934a45640d022f06db4bf3a0a84d0
skrsumit250 <skrsumit250@gmail.com>
2024-08-25 12:02:32 +0530

    updated

--- server/routes/session.js
+++ server/routes/session.js
@@ -1,25 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-
-const store = MongoStore.create({
-  mongoUrl: URI,
-  collectionName: 'sessions',
-  ttl: 24 * 60 * 60
-});
-
-router.use(session({
-  secret: 'secret_key',
-  store: store,
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false, maxAge: 3600000, httpOnly: true},
-}));
-
 const user_collection = require("../models/user");
-const doc_collection = require("../models/doctor");
+const doc_collection = require("../models/doctor"); 
 
 router.get('/', async (req, res) => {
   console.log(req.session);
