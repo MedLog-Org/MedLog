5527fb70e04493da820cf99d7a44303482f22a0c
skrsumit250 <skrsumit250@gmail.com>
2024-08-25 10:46:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ const PORT = 8000;
 
 require('dotenv').config();
 // const URI = process.env.Local_URI;
-const URI = process.env.Local_URI;
+const URI = process.env.DB_URI;
 
 mongoose.connect(URI)
   .then(() => {
